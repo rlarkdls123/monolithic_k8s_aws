6137df8c531673f75ddcc18091f75b9daf7b6aa0
unknown <lina083017@gmail.com>
2026-03-03 10:05:54 +0900

    .

---
+++ src/main/java/com/example/monolithic/order/domain/dto/OrderResponseDTO.java
@@ -0,0 +1,34 @@
+package com.example.monolithic.order.domain.dto;
+
+import com.example.monolithic.order.domain.entity.OrderEntity;
+import com.example.monolithic.order.domain.entity.OrderStatus;
+import com.example.monolithic.product.domain.dto.ProductResponseDTO;
+import com.example.monolithic.product.domain.entity.ProductEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Builder
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderResponseDTO {
+    
+    private Long id ; 
+    private Integer qty ; 
+    private OrderStatus orderStatus;
+
+    public static OrderResponseDTO fromEntity(OrderEntity entity) {
+        return OrderResponseDTO.builder()
+                .id(entity.getId())
+                .qty(entity.getQty())
+                .orderStatus(entity.getOrderStatus())
+                .build();
+
+    }
+    
+}
